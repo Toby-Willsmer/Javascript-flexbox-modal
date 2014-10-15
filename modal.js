05c0fa86e85e5d1fdd61c96e3da990e96de5d671
Toby Willsmer <toby.willsmer@nzpost.co.nz>
2014-10-16 10:34:54 +1300

    changes a tag to button for link and removes preventDefault from js, also adds default link pointer style for close link

--- modal.js
+++ modal.js
@@ -1,34 +1,34 @@
-var $modal      = $('.modal'),
-    openclse    = ['open', 'close'],
-    close_class     = ['.modal-close', '.modal-close-link'],
-    action_class    = ['.modal-link', '.modal-bg', '.modal-closer'],
-    open_modal  = function() {
-      $modal.addClass(openclse[0]);
-    },
-    close_modal = function() {
-      $modal.removeClass(openclse[0]);
-      $modal.addClass(openclse[1]);
-      $modal.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-        $modal.removeClass(openclse[1]);
-      });
-    };
+(function() {
 
-$(action_class[0]).click(function() {                                     // open modal
-  open_modal();
-});
+  var $modal        = $('.modal'),
+      openclse      = ['open', 'close'],
+      close_class   = ['.modal-close', '.modal-close-link'],
+      action_class  = ['.modal-link', '.modal-bg', '.modal-closer'],
+      open_modal  = function() {
+        $modal.addClass(openclse[0]);
+      },
+      close_modal = function() {
+        $modal.removeClass(openclse[0]);
+        $modal.addClass(openclse[1]);
+        $modal.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+          $modal.removeClass(openclse[1]);
+        });
+      };
 
-$(action_class[2]).prepend( $('<a href="#" class="modal-close"></a>') );  // adds 'close' to modal-container if class 'modal-closer' is in HTML
+  $(action_class[0]).click(function() {                             // open modal
+    open_modal();
+  });
 
-$(close_class.join() + ', ' + action_class[1]).click(function() {         // close modal
-  close_modal();
-});
+  $(action_class[2]).prepend( $('<a class="modal-close"></a>') );   // adds 'close' to modal-container if class 'modal-closer' is in HTML
 
-$('[class*="modal-"').click(function(e) {                                 // removes default anchor behavior
-  e.preventDefault();
-});
-
-$(document).keyup(function(e) {                                           // uses escape key to close modal
-  if (e.keyCode == 27) { 
+  $(close_class.join() + ', ' + action_class[1]).click(function() { // close modal
     close_modal();
-  } 
-});
+  });
+
+  $(document).keyup(function(e) {                                   // uses escape key to close modal
+    if (e.keyCode == 27) { 
+      close_modal();
+    } 
+  });
+
+})();
